b2ed48148651268cf80b02acb24da818c0ec07af
treigenborn <tobias.reigenborn@despegar.com>
2024-05-08 23:37:48 -0300

    servicioAutorizacionProvisorio

--- SGE.Consola/Program.cs
+++ SGE.Consola/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SGE.Aplicacion;
+using SGE.Repositorios;
+using System;
+
+namespace SGE.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sistema de Gestión de Expedientes");
+
+            Expediente expediente = new Expediente();
+            expediente.Caratula = "Caratula";
+                      
+        }
+    }
+}
