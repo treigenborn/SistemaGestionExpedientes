81d83c2b2486a38de0bd355ac58ddc86818d7ef9
Giancca <114377978+Giancardonee@users.noreply.github.com>
2024-05-10 19:19:06 -0300

    actualizaciion

--- SGE.Aplicacion/Servicios/ServicioAutorizacionProvisorio.cs
+++ SGE.Aplicacion/Servicios/ServicioAutorizacionProvisorio.cs
@@ -4,18 +4,6 @@ public class ServicioAutorizacionProvisorio
 {
   public bool TienePermiso(int IdUsuario, Permiso permiso)
     {   
-        try
-        {
-        if (IdUsuario == 1)
-        {
-            return true;
-        }
-            throw new AutorizacionException();            
-        }
-        catch (AutorizacionException e)
-        {
-            Console.WriteLine(e);
-            return false;
-        }
+        return IdUsuario == 1; 
     }
 }
